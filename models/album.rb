25008b0d4e89fb3491ce4ad4cab1e7c46725eb58
Simon Atkins <this-is-simon@github.com>
2018-03-21 17:18:10 +0000

    Added attributes to Album class

--- models/album.rb
+++ models/album.rb
@@ -1,2 +1,14 @@
 require('pg')
 require_relative('../db/sql_runner')
+
+class Album
+
+  attr_reader :id, :title, :genre, :artist_id
+  def initialize(options)
+    @id = options['id'].to_i if options['id']
+    @title = options['title']
+    @genre = options['genre']
+    @artist_id = options['id'].to_i if options['id']
+  end
+  
+end
