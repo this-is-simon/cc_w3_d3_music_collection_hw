604662fce2248ea2e52c98fca6220a9aa6ecef88
Simon Atkins <this-is-simon@github.com>
2018-03-21 17:25:29 +0000

    Added Album instances to console

--- console.rb
+++ console.rb
@@ -6,6 +6,8 @@ artist1 = Artist.new('name' => 'Lou Reed')
 artist2 = Artist.new('name' => 'Led Zeppelin')
 artist3 = Artist.new('name' => 'Nirvana')
 
+album1 = Album.new('title' => 'Satellite of Love', 'genre' => 'pop rock', 'artist_id' => artist1.id)
+
 Artist.delete_all
 
 artist1.save
--- models/album.rb
+++ models/album.rb
@@ -10,5 +10,21 @@ class Album
     @genre = options['genre']
     @artist_id = options['id'].to_i if options['id']
   end
-  
+
+  def save
+    sql = "INSERT INTO albums (
+    title, genre, artist_id
+    )
+    VALUES
+    (
+    $1, $2, $3
+    )
+    RETURNING id"
+
+    values = [@title, @genre, @artist_id]
+    result = SqlRunner.run(sql, values)
+    @id = result[0]['id'].to_i
+
+  end
+
 end
